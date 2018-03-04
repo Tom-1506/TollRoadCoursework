41cab55bbeeacd26c7d22e40bb97dd9ba8c49571
Tom <1599tm@gmail.com>
2018-03-04 16:25:17 +0000

    Started TollRoadMain

---
+++ src/TollRoadMain.java
@@ -0,0 +1,17 @@
+public class TollRoadMain
+{
+    public void initialiseTollRoadFromFile()
+    {
+
+    }
+
+    public void simulateFromFile(TollRoad road)
+    {
+
+    }
+
+    public static void main(String[] args)
+    {
+
+    }
+}
